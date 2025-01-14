feab7203a2a292688ab272cd2d371381e4aa2f7d
Álvaro Torrado Paños <alvaro.torrado@educa.madrid.org>
2025-01-14 20:06:27 +0100

    2

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,4 @@
-from typing import Union
-
 from fastapi import FastAPI, Request
-
-
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -10,31 +6,16 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
+@app.get("/laliga", response_class=HTMLResponse)
+async def laliga(request: Request):
+    return templates.TemplateResponse("laliga.html", {"request": request})
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/test", response_class=HTMLResponse)
-async def test(request: Request):
-    
-   
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={"nombre": "pepe"}                                                      
-    )
-
-@app.get("/items", response_class=HTMLResponse)
-async def read_item(request: Request):
-    
-    students = [ {"nombre": "pepe", "edad": 20,"score": 50}, {"nombre": "pepe", "edad": 20,"score": 50}, {"nombre": "pepe", "edad": 20,"score": 90}]
-    return templates.TemplateResponse(
-        request=request, name="students.html", context={"nombre": "pepe","students": students}                                                      
-    )
-
-#hola
+@app.get("/premierleague", response_class=HTMLResponse)
+async def premier(request: Request):
+    return templates.TemplateResponse("premierleague.html", {"request": request})
